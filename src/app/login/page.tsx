0af211fe2455fe2fadc987dc89740ae9193d8925
Marcus Stegberg <marcus@stegberg.se>
2026-02-08 22:34:08 +0100

    Refactor Login page to Server Component to fix build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,6 @@
-"use client";
+import { LoginClient } from "./LoginClient";
 
-import dynamic from "next/dynamic";
-import { Loader2 } from "lucide-react";
-
-const LoginClient = dynamic(() => import("./LoginClient").then(mod => mod.LoginClient), {
-    ssr: false,
-    loading: () => (
-        <div className="flex min-h-screen items-center justify-center">
-            <Loader2 className="animate-spin text-indigo-500" size={48} />
-        </div>
-    )
-});
+export const dynamic = "force-dynamic";
 
 export default function LoginPage() {
     return <LoginClient />;
